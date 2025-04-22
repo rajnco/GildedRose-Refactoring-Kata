f9c6c2d3c0843a217b4e8e94287cfe28fe4c1fd4
Rajkumar N <rajkumar.natarajan@gmail.com>
2025-04-22 11:40:22 +0530

    updated

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,34 +7,32 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+            match item.name:
+                case "Sulfuras, Hand of Ragnaro": # legendary item, never has to be sold or decreases in `Quality`
+                    pass
+                case "Aged Brie":
+                    item.quality = item.quality+1 if item.sell_in > 0 else item.quality+2
+                    item.quality = 50 if item.quality > 50 else item.quality
+                    item.sell_in += -1
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in > 10:
+                        item.quality = 50 if item.quality+1 > 50 else item.quality+1
+                    elif item.sell_in > 5: 
+                        item.quality = 50 if item.quality+2 > 50 else item.quality+2
+                    elif item.sell_in > 0: 
+                        item.quality = 50 if item.quality+3 > 50 else item.quality+3
                     else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+                        item.quality = 0
+                    item.sell_in += -1
+                case "Conjured":
+                    item.quality = item.quality-2 if item.sell_in > 0 else item.quality-4
+                    item.quality = 0 if item.quality < 0 else item.quality
+                    item.sell_in += -1
+                case _:     # defaut - other items
+                    item.quality = item.quality-1 if item.sell_in > 0 else item.quality-2
+                    item.quality = 0 if item.quality < 0 else item.quality
+                    item.sell_in += -1
+            
 
 class Item:
     def __init__(self, name, sell_in, quality):
